075d07610af46c1be8efea3a0e8c8f9c0fb07292
KyuSSeo <kyuri2665@gmail.com>
2025-06-13 17:28:09 +0900

    [Chore] 상태에 따라서 판정의 여부 변경
    preview 상태일 때, 오브젝트를 판정하여 풀에 반환하는 경우가 없도록 변경

--- Assets/Scripts/CheakDask.cs
+++ Assets/Scripts/CheakDask.cs
@@ -6,16 +6,21 @@ public class CheakDask : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        if (GameManager.Instance.State == GameState.Preview)
+            return;
+
         var fake = other.GetComponent<FakeObject>();
         if (fake != null)
         {
             if (fake.isFake)
             {
                 Debug.Log("정답!");
+                GameManager.Instance.Score.AddScore();
             }
             else
             {
                 Debug.Log("오답!");
+                GameManager.Instance.Score.FailAttempt();
             }
 
             other.gameObject.SetActive(false);
