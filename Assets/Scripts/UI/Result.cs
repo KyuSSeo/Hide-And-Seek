a715e8f9e928f95f35f24e0a57eaebaf33e66a64
KyuSSeo <kyuri2665@gmail.com>
2025-06-17 09:34:21 +0900

    [Test] 버튼 테스트
    활성화 비활성화 확인 완료
    
    버튼 인식이 안되는 듯 함

--- Assets/Scripts/UI/Result.cs
+++ Assets/Scripts/UI/Result.cs
@@ -1,18 +1,25 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Result : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI resultText;
+    [SerializeField] private Button restartButton;
+    [SerializeField] private Button titleButton;
 
     public void ShowResult(bool result)
     {
         resultText.text = result ? "Win" : "Lose";
         resultText.gameObject.SetActive(true);
+        titleButton.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
     }
 
     public void HideResult()
     {
+        titleButton.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false);
         resultText.gameObject.SetActive(false);
     }
 }
